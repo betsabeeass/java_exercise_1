d85c1de86f68f4dd8a69defeac2d348b8083c31a
betsabee.assayag <betsabee.assayag@epita.fr>
2024-10-23 18:38:42 +0200

    Part 5

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,11 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static jdk.jfr.consumer.EventStream.openFile;
 
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Java Programming");
         Scanner scanner = new Scanner(System.in);
 
-        for (; ; ) {
+        for (; ;) {
             String input = scanner.nextLine();
             if (input.equals("quit")) {
                 break;
@@ -17,21 +28,41 @@ public class Launcher {
                 System.out.println(fibonacci(number));
 
             }
-            else {
+            if (input.equals("freq")) {
+                System.out.println("Quel est le chemin du fichier: ");
+                Path filePath = Paths.get(scanner.nextLine());
+
+                try {
+                    String content = Files.readString(filePath);
+                    String ThreeWord = findTopThreeWord(content);
+                    System.out.println(" " +ThreeWord);
+
+
+                } catch (Exception e) {
+                    System.out.println("Unreadable file");
+
+                }
+
+            } else {
                 System.out.println("Unknown command");
             }
         }
     }
+
     public static int fibonacci(int n) {
         if (n == 0) {
             return 0;
-        }
-        else if (n == 1) {
+        } else if (n == 1) {
             return 1;
-        }
-        else {
+        } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
+    }
+
+    public static String findTopThreeWord(String content) {
+        String[] words = content.replaceAll("\\p{Punct}", "").toLowerCase().split("\\s+");
+        Map<String, Long> wordCount = Arrays.stream(words).filter(word -> !word.isBlank()).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return wordCount.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())).limit(3).map(Map.Entry::getKey).collect(Collectors.joining(" "));
     }
 }
 
