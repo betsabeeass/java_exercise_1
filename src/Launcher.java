17c293b5366d494d1d28bc034bcee008260e3d9c
betsabee.assayag <betsabee.assayag@epita.fr>
2024-10-23 15:44:17 +0200

    Part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,20 +5,38 @@ public class Launcher {
         System.out.println("Welcome to Java Programming");
         Scanner scanner = new Scanner(System.in);
 
-        for (; ;){
-
+        for (; ; ) {
             String input = scanner.nextLine();
-            if (input.equals("quit"))
+            if (input.equals("quit")) {
                 break;
-            else{
+            }
+            if (input.equals("fibo")) {
 
+                System.out.println("Enter the number: ");
+                int number = scanner.nextInt();
+                System.out.println(fibonacci(number));
+
+            }
+            else {
                 System.out.println("Unknown command");
             }
-
         }
     }
-
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
+
+
+
 
 
 
